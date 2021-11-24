84e8a401d18b00f1dbc5b43cde5f6303597871e9
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-24 16:04:15 +0100

    init

---
+++ dadi/js/script.js
@@ -0,0 +1,11 @@
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+
+Consigli del giorno:
+Iniziate dalla parte base, se tutto funziona passiamo a fare dei check su casi limite, fatto questo passiamo a vedere se esistono dei metodi di JS che possono aiutarci.
+I bonus sono sempre da fare in cartella separata. */
