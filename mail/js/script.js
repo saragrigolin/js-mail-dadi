215135791ad812d628242bde4128659cd8c8fe4c
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-24 15:15:50 +0100

    add script to verify email

--- mail/js/script.js
+++ mail/js/script.js
@@ -11,3 +11,31 @@ Consigli del giorno:
 Iniziate dalla parte base, se tutto funziona passiamo a fare dei check su casi limite, fatto questo passiamo a vedere se esistono dei metodi di JS che possono aiutarci.
 I bonus sono sempre da fare in cartella separata.*/
 
+//chiedo all'utente di inserire la propria email
+const mail = prompt('Inserisci la tua email');
+
+//creo un array di indirizzi mail
+const mails = ['mariorossi@gmail.com', 'mariorossi82@gmail.com', 'mariaverdi@gmail.com', 'mariaverdi@outlook.it'];
+console.log(mails);
+
+let span = document.querySelector('span');
+let find = false;
+
+//creo un ciclo for che cerchi 
+for (let index = 0; index < mails.length; index++) {
+    const element = mails[index];
+
+    //controllo se un elemento dell'array è uguale alla mail inserita dall'utente
+    if (mail.toLowerCase() == element.toLowerCase()) {
+        find = true;
+    }
+}
+
+//se l'indirizzo mail inserito dall'utente corrisponde a uno dell'array, allora
+if (find) {
+    console.log('Indirizzo email presente nella lista');
+    span.innerHTML = 'Indirizzo mail ' + mail + ' presente nella lista';
+} else { //altrimenti
+    console.log('Indirizzo email non presente nella lista');
+    span.innerHTML = 'Indirizzo mail ' + mail + ' non presente nella lista';
+}
