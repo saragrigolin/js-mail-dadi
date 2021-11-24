7ab3c6a4e0acba26c4ed537941f28e8764899f94
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-24 14:57:01 +0100

    init

---
+++ mail/js/script.js
@@ -0,0 +1,13 @@
+/* Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. 
+
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+
+Consigli del giorno:
+Iniziate dalla parte base, se tutto funziona passiamo a fare dei check su casi limite, fatto questo passiamo a vedere se esistono dei metodi di JS che possono aiutarci.
+I bonus sono sempre da fare in cartella separata.*/
+
